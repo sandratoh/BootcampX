89120a40d8f357b228d704659bcf07b3c5d98e1a
Sandra To <sandra.to.e@gmail.com>
2021-03-02 22:35:59 +0000

    Edit indentation for readability

--- 4_queries/6_cohort_with_longest_ar.sql
+++ 4_queries/6_cohort_with_longest_ar.sql
@@ -13,7 +13,7 @@ HAVING AVG(completed_at - started_at) >= ALL (
                                               JOIN students ON students.id = assistance_requests.student_id
                                               JOIN cohorts ON cohorts.id = cohort_id
                                               GROUP BY cohorts.name
-                                              );
+                                            );
 
 /* ORDER BY and LIMIT method (Compass) */
 /*
