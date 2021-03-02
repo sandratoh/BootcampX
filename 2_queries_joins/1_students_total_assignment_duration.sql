1f71452b1dc11c6b70f379e10fd31424c40ec101
Sandra To <sandra.to.e@gmail.com>
2021-03-02 00:44:57 +0000

    Remove unnecessary indentation to follow sql best practice

--- 2_queries_joins/1_students_total_assignment_duration.sql
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -1,4 +1,4 @@
 SELECT SUM(assignment_submissions.duration) AS total_duration
-  FROM assignment_submissions
-  JOIN students ON students.id = student_id
-  WHERE students.name = 'Ibrahim Schimmel';
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -1,5 +1,5 @@
 SELECT SUM(assignment_submissions.duration) as total_duration
-  FROM assignment_submissions
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name = 'FEB12';
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12';
